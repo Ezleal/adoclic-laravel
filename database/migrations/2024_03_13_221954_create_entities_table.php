4eb56dc4b0c1069355c57a9f09516c1af3a521e2
Ezequiel Almeira <ezequiel.almeira@buscouniversidad.com.ar>
2024-03-13 19:28:34 -0300

    ✨[Migrations] Add entities and categories table

--- database/migrations/2024_03_13_221954_create_entities_table.php
+++ database/migrations/2024_03_13_221954_create_entities_table.php
@@ -9,10 +9,15 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('entities', function (Blueprint $table) {
             $table->id();
+            $table->string('api');
+            $table->text('description');
+            $table->string('link');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
